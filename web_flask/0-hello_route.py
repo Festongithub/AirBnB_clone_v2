9eb6d86f42452f640c9a2b558570f54d1b2f9dc6
Festongithub <fkemboik@gmail.com>
2024-06-03 03:34:21 -0700

    Web Flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
+"""script that Starts a flask web application"""
+
 from flask import Flask
 
-"""Starts a flask web application"""
-
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
+@app.route('/')
+def index():
     """displays hello HBNB"""
     return "Hello HBNB"
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
