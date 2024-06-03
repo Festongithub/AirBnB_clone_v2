c8655ce76179d8ec7d14c186bd823b57613e28b5
Festongithub <fkemboik@gmail.com>
2024-06-03 04:39:39 -0700

    Web Flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -14,5 +14,13 @@ def index():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    returns the HNBNB
+    """
+    return "HBNB"
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
