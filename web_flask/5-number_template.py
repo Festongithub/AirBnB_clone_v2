d38f770f2db5f322802ca00386d4c01aacf63cc0
Festongithub <fkemboik@gmail.com>
2024-06-03 06:25:17 -0700

    Web Flask

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""script that Starts a flask web application"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    displays hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    returns the HNBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_cool(text):
+    """
+    Displays  c
+    """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'},
+           strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def magic_python(text):
+    """
+    Displays magicity of python
+    """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Displays n as an integer
+    """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Renders  a html template
+    """
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
