41d887fb4acf32d3a3d8816f1f8396c2e7feb433
Festongithub <fkemboik@gmail.com>
2024-06-03 05:14:28 -0700

    Web Flask

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""script that Starts a flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    displays hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    returns the HNBNB
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_cool(text):
+    """
+    Displays  c
+    """
+    return "C " + text.replace('_', ' ')
+
+
+@app.route('/python/', defaults={'text': 'is cool'},
+           strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def magic_python(text):
+    """
+    Displays magicity of python
+    """
+    return "Python " + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Displays n as an integer
+    """
+    return f"{n} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
