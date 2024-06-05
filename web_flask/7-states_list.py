d7d8c3bde82cfaf0435dae66254647c15a46e9c4
Festongithub <fkemboik@gmail.com>
2024-06-05 05:15:12 -0700

    Base

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""
+imports list of states
+"""
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.teardown_appcontext
+def db_teardown(exception=None):
+    """
+    SQL alchemy session teardonw
+    """
+    if storage is not None:
+        storage.close()
+
+
+@app.route('/states_list')
+
+def list_states(n=None):
+    """
+    display list in HTML format
+    """
+    states = storag.all('State')
+    return render_template('7-states_list.html', states=states)
+
+if __name__ == '__main__':
+    app.run(debug=True)
